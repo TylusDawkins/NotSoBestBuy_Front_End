970d8f34c83a71be2411a67ddecdf7957a521b62
dealhouse <de.juan1@live.com>
2022-04-22 07:59:50 -0400

    fixed classname error

--- client/src/components/Filter.js
+++ client/src/components/Filter.js
@@ -12,7 +12,7 @@ const Filter = (props) => {
         let categoryName = categories.map((category) => {
             return [category.id, category.name]
         })
-        
+        console.log(categoryName)
         let nameObj = Object.fromEntries(categoryName)
     
         let catIdArray = props.searchResults.map((category) => {
@@ -37,36 +37,36 @@ const Filter = (props) => {
                     {<h5>Categories</h5>}
                 {catIdObjectKeys.map((category) => (
                     <div>
-                        <Link classname="filter-link" onClick={handleClickCat} to={`/search/${catIdObject[category]}/${val}`}><label>{category}</label></Link>
+                        <Link className="filter-link" onClick={handleClickCat} to={`/search/${catIdObject[category]}/${val}`}><label>{category}</label></Link>
                     </div>
                 ))}
                 </div>
                     <div>
                     <h5>Price</h5>
-                <Link classname="filter-link" onClick={handleClickPrice} to={`/search/${id}/200`}> <label>$200 or less</label></Link>
+                <Link className="filter-link" onClick={handleClickPrice} to={`/search/${id}/200`}> <label>$200 or less</label></Link>
                     </div>
                     <div>
-                <Link classname="filter-link" onClick={handleClickPrice} to={`/search/${id}/400`}><label>$400 or less</label></Link>
+                <Link className="filter-link" onClick={handleClickPrice} to={`/search/${id}/400`}><label>$400 or less</label></Link>
                     
                     </div>
                     <div>
-                <Link classname="filter-link" onClick={handleClickPrice} to={`/search/${id}/600`}><label>$600 or less</label></Link>
+                <Link className="filter-link" onClick={handleClickPrice} to={`/search/${id}/600`}><label>$600 or less</label></Link>
                     
                     </div>
                     <div>
-                <Link classname="filter-link" onClick={handleClickPrice} to={`/search/${id}/800`}><label>$800 or less</label></Link>
+                <Link className="filter-link" onClick={handleClickPrice} to={`/search/${id}/800`}><label>$800 or less</label></Link>
                     
                     </div>
                     <div>
-                <Link classname="filter-link" onClick={handleClickPrice} to={`/search/${id}/1000`}><label>$1000 or less</label></Link>
+                <Link className="filter-link" onClick={handleClickPrice} to={`/search/${id}/1000`}><label>$1000 or less</label></Link>
                     
                     </div>
                     <div>
-                <Link classname="filter-link" onClick={handleClickPrice} to={`/search/${id}/2000`}><label>$2000 or less</label></Link>
+                <Link className="filter-link" onClick={handleClickPrice} to={`/search/${id}/2000`}><label>$2000 or less</label></Link>
                     
                 </div>
                     <div>
-                <Link classname="filter-link" onClick={handleClickPrice} to={`/search/${id}/2001`}><label>$2000 or more</label></Link>
+                <Link className="filter-link" onClick={handleClickPrice} to={`/search/${id}/2001`}><label>$2000 or more</label></Link>
                     
                 </div>
             </aside>

1c6c8dd841c2aa04bad7ce74d93cfc273514f794
dealhouse <de.juan1@live.com>
2022-04-22 14:07:30 -0400

    added elements to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,30 @@
+import axios from "axios";
+import { useEffect } from "react";
+import '../styles/Home.css'
 const Home = () => {
+    let product
+    useEffect(() => {
+        const getProduct = async () => {
+            let rand = Math.floor(Math.random() * 9)
+            console.log(rand)
+            const product = await axios.get(`https://notsobestbuy-front-end.herokuapp.com/product/${rand}`)
+        }
+        getProduct()
+    }, [])
+    console.log(product)
     return (
         <div>
+
             <h1>Home</h1>
+            <h4>Featured</h4>
+            {product !== undefined && 
+            <div>
+                
+                <h5>{product.name}</h5>
+                <h5>${product.price}</h5>
+                <img classname="home-image" src={product.image} alt={product.name}/>
+            </div>
+                }
         </div>
     );
 }
